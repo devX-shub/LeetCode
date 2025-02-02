c3b2d5e36a630a33016ea838fefbc7f60ca820ef
devX-shub <161881953+devX-shub@users.noreply.github.com>
2025-02-03 05:04:16 +0530

    Update 0062-unique-paths.cpp

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+/*
+    Brute force 
+    int countPath(int m,int n, int i ,int j)
+    {
+        if(i == m-1 && j == n - 1)
+            return 1;
+        if(i >= m || j >= n)
+            return 0;
+        return countPath(m,n,i+1,j) + countPath(m,n,i,j+1);
+    }
+    
+    int uniruePaths(int m,int n)
+    {
+        int i = 0, j = 0;
+        return countPath(m,n,i,j);
+    }
+*/
+//Most optimal solution.
     int uniquePaths(int m, int n) {
         int N = m + n - 2;
         int r = m-1;
