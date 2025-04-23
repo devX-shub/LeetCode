3a07940fc7d9127487a0306040572e0e8a6dd7da
devX-shub <161881953+devX-shub@users.noreply.github.com>
2025-04-23 23:14:55 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int idx = 0;
+       for(int i = 0; i < nums.size();i++)
+       {
+        if(nums[i] == 0)
+            {
+                continue;
+            }
+        else{
+            swap(nums[i],nums[idx]);
+            idx++;
+        }
+       }
+    }
+};
